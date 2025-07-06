eb04b36605c09082a36900f2c264e90a486419bf
Yogendra Singh Rathod <rathodyogi91221@gmail.com>
2025-07-06 22:53:09 +0530

    Update User.js

--- Server/src/routes/User.js
+++ Server/src/routes/User.js
@@ -1,11 +1,11 @@
 // Import the required modules
 const express = require("express");
 const router = express.Router(); // Single router
-const {resetPassword,resetPasswordToken}=require('../controllers/resetPassword')
+// const {resetPassword,resetPasswordToken}=require('../controllers/resetPassword')
 
 
 // Import the required controllers and middleware functions
-const { login, signup ,changePassword,logout, firebaseSignup, testFirebase } = require("../controllers/AuthController");
+const { login, signup ,logout, firebaseSignup, testFirebase } = require("../controllers/AuthController");
 const { auth, isAdmin, isInstructor, isStudent } = require("../middlewares/middlewares");
 
 // Define routes
@@ -13,11 +13,11 @@ router.post("/login", login);    // Route for login
 router.post("/signup", signup);  // Route for signup
 router.post("/firebase-signup", firebaseSignup);  // Route for Firebase signup
 router.get("/test-firebase", testFirebase);  // Route for testing Firebase
-router.post("/changePassword",auth, changePassword)   //route to change password
+// router.post("/changePassword",auth, changePassword)   //route to change password
 router.post("/logout",logout)  //route for logout
 
-router.post("/reset-password-token", resetPasswordToken) //route for restPasswordToken
-router.post("/reset-password", resetPassword)  //Route for reset password
+// router.post("/reset-password-token", resetPasswordToken) //route for restPasswordToken
+// router.post("/reset-password", resetPassword)  //Route for reset password
 
 // Export the router
 module.exports = router;
