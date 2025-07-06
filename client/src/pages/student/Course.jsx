2f72c5047c1139348f931bfe9c744f6eb1b7d495
Yogendra Singh Rathod <rathodyogi91221@gmail.com>
2025-07-07 00:34:30 +0530

    Final Done

--- client/src/pages/student/Course.jsx
+++ client/src/pages/student/Course.jsx
@@ -8,7 +8,7 @@ const Course = ({course}) => {
   // console.log(course);
   
   return (
-    <Link to={`course-detail/${course._id}`}>
+    <Link to={`/course-detail/${course._id}`}>
     <Card className="overflow-hidden rounded-xl dark:bg-gray-800 bg-white shadow-lg hover:shadow-2xl transform hover:scale-105 transition-all duration-300 border border-gray-100">
       <div className="relative">
         <div className="absolute top-4 right-4 z-10">
