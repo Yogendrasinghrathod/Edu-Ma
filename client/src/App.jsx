eae81b370a65ed9cbd353584b8f3b7e1abcf6072
Yogendra Singh Rathod <rathodyogi91221@gmail.com>
2025-07-06 21:48:31 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -43,9 +43,7 @@ const appRouter = createBrowserRouter([
       {
         path: "/login",
         element: (
-          <AuthenticatedUser>
             <Login />
-          </AuthenticatedUser>
         ),
       },
       {
