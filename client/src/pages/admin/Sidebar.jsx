3e0b001faa0bc12be7fb72545ed3d5ec9de4a01f
Yogendra Singh Rathod <rathodyogi91221@gmail.com>
2025-08-15 00:59:59 +0530

    Update Sidebar.jsx

--- client/src/pages/admin/Sidebar.jsx
+++ client/src/pages/admin/Sidebar.jsx
@@ -4,8 +4,8 @@ import { Link, Outlet } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className="flex md:flex-row dark:bg-gray-700  ">
-      <div className="hidden lg:block w-[250px] sm:w-[300px] space-y-8 border-r border-r-gray-300 dark:border-r-gray-700  bg-[#f0f0f0] p-5 sticky top-0 h-screen my-0 dark:bg-gray-700 dark:text-white md:">
+    <div className="flex dark:bg-gray-700  ">
+      <div className="hidden lg:block w-[250px] sm:w-[300px] space-y-8 md:space-y-2 border-r border-r-gray-300 dark:border-r-gray-700  bg-[#f0f0f0] p-5 sticky top-0 h-screen my-0 dark:bg-gray-700 dark:text-white md:">
         <div className="space-y-4 mt-20 ">
           <Link to="/admin/dashboard" className="flex items-center gap-2">
             <ChartNoAxesColumn size={22} />
